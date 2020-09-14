54fdf14dd78a11f175c98077ed69377550d7d019
Chris Southwick <csouthwick@live.com>
2020-09-13 22:01:42 -0400

    generate HTML for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var cityInputEl = document.getElementById("city-input");
+var weatherEl = document.getElementById("weather");
 var apiKey = "b0c7f7890fd4b66f3bf5aff8377ec0db";
 
 function getWeatherCoords() {
@@ -39,13 +40,41 @@ function getWeather(name, latitude, longitude) {
 }
 
 function displayWeather(name, weatherData) {
-  var date = moment.unix(weatherData.current.dt);
-  console.log(name + " (" + date.format("M/D/YYYY") + ")");
-  console.log("http://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + "@2x.png");
-  console.log("Temperature: " + weatherData.current.temp + " °F");
-  console.log("Humidity: " + weatherData.current.humidity + "%");
-  console.log("Wind Speed: " + weatherData.current.wind_speed + " MPH");
-  console.log("UV Index: " + weatherData.current.uvi);
+  // start of current weather card
+  var tempHTML = "<div class='card'><div class='card-body'>";
+  var date = moment.unix(weatherData.current.dt).format("M/D/YYYY");
+
+  // card title with city name, date, and weather icon
+  tempHTML += "<h2 class='card-title'>" + name + " (" + date + ") ";
+  tempHTML += "<img src='http://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + ".png' alt='" + weatherData.current.weather[0].description + "' /></h3>";
+
+  // temp, humidity, wind speed, and start of UV index
+  tempHTML += "<p class='card-text'>Temperature: " + weatherData.current.temp + " &deg;F</p>";
+  tempHTML += "<p class='card-text'>Humidity: " + weatherData.current.humidity + "%</p>";
+  tempHTML += "<p class='card-text'>Wind Speed: " + weatherData.current.wind_speed + " MPH</p>";
+  tempHTML += "<p class='card-text'>UV Index: <span class='badge ";
+
+  // Determine color for UV scale, added as a class name
+  // Only need to test if uvi is lower than the next threshold as earlier if statements will catch the lower bounds. Last check is for any other number which is automatically 11+
+  if (weatherData.current.uvi < 3) {
+    tempHTML += "low-uv";
+  } else if (weatherData.current.uvi < 6) {
+    tempHTML += "moderate-uv";
+  } else if (weatherData.current.uvi < 8) {
+    tempHTML += "high-uv";
+  } else if (weatherData.current.uvi < 11) {
+    tempHTML += "very-high-uv";
+  } else {
+    tempHTML += "extreme-uv";
+  }
+  tempHTML += "'>" + weatherData.current.uvi + "</span></p>";
+
+  // close the current weather card
+  tempHTML += "</div></div>";
+
+  // test current progress
+  weatherEl.innerHTML = tempHTML;
+
   for (var i = 1; i < 6; i++) {
     date = moment.unix(weatherData.daily[i].dt);
     console.log(date.format("M/D/YYYY"));
